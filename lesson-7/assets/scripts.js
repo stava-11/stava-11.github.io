6468ba838f81087e664fd8a90c3db4e74c1b513e
stava-11 <77128788+stava-11@users.noreply.github.com>
2021-02-18 22:51:45 -0700

    Add files via upload

---
+++ lesson-7/assets/scripts.js
@@ -0,0 +1,44 @@
+
+
+
+// select desired query selector
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+// swap data-src with src
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+// intersector observor which loads image as user scrolls over them
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
+// set dates for footer 
+var dayOfWeek = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var monthOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var d = new Date();
+var n = d.getDay();
+var dayOfMonth = d.getDate();
+var month = d.getMonth() + 1;
+var year = d.getFullYear();
+var day = dayOfWeek[n];
+document.getElementById('lastUpdate').innerHTML = (day + ', ' + dayOfMonth + ' ' + monthOfYear[month-1] + ' ' + year + '.');
+document.getElementById('year').innerHTML = year;
